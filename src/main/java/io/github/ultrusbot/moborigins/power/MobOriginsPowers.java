263826e5701df0bf6a9022ccc44af43a857cd62c
Ultra <13109003+UltrusBot@users.noreply.github.com>
2021-02-25 22:02:39 -0500

    Guardians no longer attack those with the Guardian/Elder Guardian origin

--- src/main/java/io/github/ultrusbot/moborigins/power/MobOriginsPowers.java
+++ src/main/java/io/github/ultrusbot/moborigins/power/MobOriginsPowers.java
@@ -13,6 +13,7 @@ public class MobOriginsPowers {
     public static final PowerType<Power> RIDEABLE_CREATURE = new PowerTypeReference(new Identifier("moborigins", "rideable_creature"));
     public static final PowerType<Power> BOUNCY = new PowerTypeReference(new Identifier("moborigins", "bouncy"));
     public static final PowerType<Power> ALPHA_WOLF = new PowerTypeReference(new Identifier("moborigins", "alpha_wolf"));
+    public static final PowerType<Power> GUARDIAN_ALLY = new PowerTypeReference(new Identifier("moborigins", "guardian_ally"));
 
 
 
