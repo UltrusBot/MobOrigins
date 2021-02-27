a2a82299dc5ff17c390da475813e3400494dd55f
Ultra <13109003+UltrusBot@users.noreply.github.com>
2021-02-26 22:59:04 -0500

    Fixed Crash in build

--- src/main/java/io/github/ultrusbot/moborigins/mixin/GuardianMixin.java
+++ src/main/java/io/github/ultrusbot/moborigins/mixin/GuardianMixin.java
@@ -2,6 +2,7 @@ package io.github.ultrusbot.moborigins.mixin;
 
 import io.github.ultrusbot.moborigins.power.MobOriginsPowers;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.mob.GuardianEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,11 +13,11 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(targets = "net.minecraft.entity.mob.GuardianEntity$FireBeamGoal")
-public class GuardianMixin {
+public abstract class GuardianMixin extends Goal {
 
     @Shadow @Final private GuardianEntity guardian;
 
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/GuardianEntity;getTarget()Lnet/minecraft/entity/LivingEntity;", shift = At.Shift.AFTER))
+    @Inject(method = "Lnet/minecraft/entity/mob/GuardianEntity$FireBeamGoal;tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/GuardianEntity;getTarget()Lnet/minecraft/entity/LivingEntity;", shift = At.Shift.AFTER))
     public void tick$MobOrigins(CallbackInfo ci) {
         LivingEntity livingEntity = this.guardian.getTarget();
         if (MobOriginsPowers.GUARDIAN_ALLY.isActive(livingEntity)) {
