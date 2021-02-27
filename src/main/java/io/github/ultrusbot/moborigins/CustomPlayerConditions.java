f3b303bce6a68d893b35b67aa9c3bc7c7df37ac3
Ultra <13109003+UltrusBot@users.noreply.github.com>
2021-02-27 16:23:55 -0500

    Began work on True Phantom

--- src/main/java/io/github/ultrusbot/moborigins/CustomPlayerConditions.java
+++ src/main/java/io/github/ultrusbot/moborigins/CustomPlayerConditions.java
@@ -7,7 +7,10 @@ import io.github.apace100.origins.util.Comparison;
 import io.github.apace100.origins.util.SerializableData;
 import io.github.apace100.origins.util.SerializableDataType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.registry.Registry;
 
 public class CustomPlayerConditions {
@@ -19,6 +22,12 @@ public class CustomPlayerConditions {
                 (data, player) -> ((Comparison)data.get("comparison")).compare(player.world.getBiome(player.getBlockPos()).getTemperature(), data.getFloat("compare_to"))));
 
         register(new ConditionFactory<>(new Identifier(MOD_ID, "is_full_moon"), new SerializableData(), (data, player) -> player.world.getMoonSize() == 1.0));
+
+        register(new ConditionFactory<>(new Identifier(MOD_ID, "time_since_rest"), new SerializableData()
+                .add("comparison", SerializableDataType.COMPARISON)
+                .add("compare_to", SerializableDataType.INT),
+                (data, player) -> ((Comparison)data.get("comparison")).compare(MathHelper.clamp(((ServerPlayerEntity)player).getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.TIME_SINCE_REST)), 1, Integer.MAX_VALUE), data.getInt("compare_to"))));
+
 
     }
 
