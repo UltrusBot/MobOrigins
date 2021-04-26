e37879965bb694044115c2b7d314a1e7783bc8b7
ultra <13109003+UltrusBot@users.noreply.github.com>
2021-04-26 13:56:04 -0400

    Remove Biome Temp Condition, Updated to use Origins Biome Condition Check

--- src/main/java/io/github/ultrusbot/moborigins/CustomPlayerConditions.java
+++ src/main/java/io/github/ultrusbot/moborigins/CustomPlayerConditions.java
@@ -1,6 +1,5 @@
 package io.github.ultrusbot.moborigins;
 
-import io.github.apace100.origins.Origins;
 import io.github.apace100.origins.power.factory.condition.ConditionFactory;
 import io.github.apace100.origins.registry.ModRegistries;
 import io.github.apace100.origins.util.Comparison;
@@ -8,7 +7,6 @@ import io.github.apace100.origins.util.SerializableData;
 import io.github.apace100.origins.util.SerializableDataType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Identifier;
@@ -18,11 +16,6 @@ import net.minecraft.util.registry.Registry;
 public class CustomPlayerConditions {
     private static final String MOD_ID = MobOriginsMod.MOD_ID;
     public static void register() {
-        register(new ConditionFactory<>(new Identifier(MOD_ID, "biome_temperature"), new SerializableData()
-                .add("comparison", SerializableDataType.COMPARISON)
-                .add("compare_to", SerializableDataType.FLOAT),
-                (data, player) -> ((Comparison)data.get("comparison")).compare(player.world.getBiome(player.getBlockPos()).getTemperature(), data.getFloat("compare_to"))));
-
         register(new ConditionFactory<>(new Identifier(MOD_ID, "is_full_moon"), new SerializableData(), (data, player) -> player.world.getMoonSize() == 1.0));
 
         register(new ConditionFactory<>(new Identifier(MOD_ID, "time_since_rest"), new SerializableData()
