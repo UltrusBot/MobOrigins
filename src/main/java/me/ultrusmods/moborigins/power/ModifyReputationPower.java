3e5eff0fcfe9db911df1a38898c25d971c4bc3a5
ultra <13109003+UltrusBot@users.noreply.github.com>
2023-02-28 00:29:14 -0500

    Fix swapped actor/target pair in docs.

--- src/main/java/me/ultrusmods/moborigins/power/ModifyReputationPower.java
+++ src/main/java/me/ultrusmods/moborigins/power/ModifyReputationPower.java
@@ -20,9 +20,10 @@ import java.util.function.Predicate;
 
     DESC: This power type allows you to effectively modify the reputation of a villager when you interact with it.
 You can learn more about reputation [here](https://minecraft.fandom.com/wiki/Villager#Gossiping).
+!!! tip "The 'actor' is the villager, while the 'target' is the entity with the power, that is interacting with the villager."
 
     PARAMS:
-    - {bientity_condition} {Bi-entity Condition Type} {https://origins.readthedocs.io/en/latest/types/bientity_condition_types/} {optional} {This is a bi-entity condtion with the actor, the player, and the target, the villager.}
+    - {bientity_condition} {Bi-entity Condition Type} {https://origins.readthedocs.io/en/latest/types/bientity_condition_types/} {optional} {This is a bi-entity condtion with the actor, the villager, and the target, the player.}
     - {modifier} {Attribute Modifier} {https://origins.readthedocs.io/en/latest/types/data_types/attribute_modifier/} {optional} {This is a modifier that will be applied to your reputation with the villager.}
     - {modifiers} {List of Modifier Types} {https://origins.readthedocs.io/en/latest/types/data_types/attribute_modifier/} {optional} {This is a list of modifiers that will be applied to your reputation with the villager.}
 
